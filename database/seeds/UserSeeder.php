0930144c5243c67a56ad94f89d41fc8cce02780c
Chris Rouxel <chris@linkdigital.co.uk>
2021-04-28 08:59:14 +0100

    Pushback from Server

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'firstname' => 'Link',
+            'lastname'  => 'Testadmin',
+            'email'      => 'testadmin@linkdigital.co.uk',
+            'password'   => bcrypt('test'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'role' => 'admin',
+            'phone' => '1234567890',
+            'company_id' => 1,
+            'is_deleted' => null,
+            'email_verified_at' => now()
+        ]);
+
+        User::create([
+
+            'firstname' => 'Link',
+            'lastname'  => 'Testdealer',
+            'email'      => 'testdealer@linkdigital.co.uk',
+            'password'   => bcrypt('test'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'role' => 'dealer',
+            'phone' => '1234567890',
+            'company_id' => 1,
+            'is_deleted' => null,
+            'email_verified_at' => now()
+        ]);
+
+        User::create([
+            'firstname' => 'Link',
+            'lastname'  => 'Testbroker',
+            'email'      => 'testbroker@linkdigital.co.uk',
+            'password'   => bcrypt('test'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'role' => 'broker',
+            'phone' => '1234567890',
+            'company_id' => 1,
+            'is_deleted' => null,
+            'email_verified_at' => now()
+        ]);
+    }
+}
