cb1f2c3a614c720dd6aec1ac4a91e531cf8309fa
LD_ChrisRouxel <chris@linkdigital.co.uk>
2021-09-17 12:30:16 +0100

    Yet More Date Parsings

--- app/Http/Livewire/OrderForm.php
+++ app/Http/Livewire/OrderForm.php
@@ -330,8 +330,8 @@ class OrderForm extends Component
 	{
 		$this->validate();
 
-		$this->delivery_date = Carbon::createFromFormat( 'd/m/Y', $this->delivery_date );
-        $this->due_date = Carbon::createFromFormat( 'd/m/Y' , $this->due_date );
+		$this->delivery_date = new DateTime( $this->delivery_date );
+        $this->due_date = new DateTime( $this->due_date );
 
 		if ( !isset( $this->order )) {
 
