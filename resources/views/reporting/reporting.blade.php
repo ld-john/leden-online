591bc16d9340442566b879f34ad828b41c7865a2
ld-john <130744419+ld-john@users.noreply.github.com>
2023-11-08 10:46:30 +0000

    Add exception to Orders and pull into Reporting

--- resources/views/reporting/reporting.blade.php
+++ resources/views/reporting/reporting.blade.php
@@ -12,10 +12,6 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold">Downloads</h6>
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="filter_exceptions">
-                    <label class="form-check-label" for="filter_exceptions">Filter Exceptions</label>
-                </div>
             </div>
             <div class="card-body">
                 <h5>Registered—Month</h5>
